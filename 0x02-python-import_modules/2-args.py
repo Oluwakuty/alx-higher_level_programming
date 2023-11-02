8e1ca755aa07b52171c7ed8ceff826d19909d087
oluwakuty <oyindamoanu@gmail.com>
2023-11-02 15:07:48 -0700

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,15 +3,12 @@
 if __name__ == "__main__":
     import sys
 
-    arg = sys.argv
-    size = len(arg) - 1
-
-    if size > 1:
-        print("{} arguments:".format(size))
-        for i in range(1, size + 1):
-            print("{}: {}".format(i, arg[i]))
-        elif size == 0:
-            print("{} arguments.".format(size))
-        else:
-            print("{} argument:".format(size))
-            print("{}: {}".format(size, arg[1]))
+    argLen = len(sys.argv)
+    if argLen == 1:
+        print("{} arguments.".format(argLen - 1))
+    elif argLen == 2:
+        print("{} argument:".format(argLen - 1))
+    else:
+        print("{} arguments:".format(argLen - 1))
+        for i in range(1, argLen):
+             print(f"{i}: {sys.argv[i]}")
